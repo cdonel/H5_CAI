c35d4b1e4680152bd5692dcb8ac0732bbe5c5fa4
cdonel <88918165+cdonel@users.noreply.github.com>
2025-03-09 23:40:28 -0400

    Retrieve codon usage index for hosts by webscraping http://www.kazusa.or.jp/codon/

---
+++ 10_retrieve_codon_usage_index.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+import utils
+
+def main():
+    for host, url in utils.codon_usage_index_urls.items(): # Loops through dictionary containing host name and URL
+        write_path = "data/codon_usage_database/{0}.txt".format(host) # Destination of output fil
+        condon_usage_index = get_codon_usage_index(url) # Codon usage index scraped from web data using URL
+
+# Scrapes web data from codon usage database to return codon usage index
+def get_codon_usage_index(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    return soup.text
+    
+if __name__ =="__main__":
+    main()
