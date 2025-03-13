2923f580d4369f7dc7af58e309d82afba6ab8630
cdonel <88918165+cdonel@users.noreply.github.com>
2025-03-13 08:34:34 -0400

    spacing

--- 10_retrieve_codon_usage_index.py
+++ 10_retrieve_codon_usage_index.py
@@ -4,8 +4,10 @@ import utils
 
 def main():
     for host, url in utils.codon_usage_index_urls.items(): # Loops through dictionary containing host name and URL
-        write_path = "data/codon_usage_database/{0}.txt".format(host) # Destination of output fil
+        write_path = "data/codon_usage_database/test/{0}.txt".format(host) # Destination of output fil
         condon_usage_index = get_codon_usage_index(url) # Codon usage index scraped from web data using URL
+        utils.write_txt(write_path, condon_usage_index)
+        print("Retrieved {0} codon usage index.".format(host))
 
 # Scrapes web data from codon usage database to return codon usage index
 def get_codon_usage_index(url):
