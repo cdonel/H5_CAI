e6581bc81873d9d7d49e8283bc872cc9a9b7a8b3
cdonel <88918165+cdonel@users.noreply.github.com>
2025-03-17 06:02:04 -0400

    generalized host plot methods into single method

--- 31_figures.py
+++ 31_figures.py
@@ -8,153 +8,54 @@ import starbars
 def main():
     read_path = 'data/cai_results/cai_results.csv' # read file location
     cai_results = utils.csv_to_df(read_path) # csv to dataframe
-    seq_count_to_xlsx(cai_results)
-    cai_results = cai_results[cai_results["gene"].str.contains("No gene|MP|NS") == False] # Exclude genes: No gene, MP, and NS. Not enough data to include.
-    #chicken(cai_results) # Mann-Whitney U rank for H5NX chicken genes
-    #human(cai_results) # Mann-Whitney U rank for H5NX human genes
-    #duck(cai_results) # Mann-Whitney U rank for H5NX duck genes
-    #swine(cai_results) # Mann-Whitney U rank for H5NX swine genes
+    plot_host(cai_results, host='chicken', subtypes=['H5N1', 'H5N2', 'H5N6', 'H5N8'], min_y=0.7, max_y=0.9)
+    plot_host(cai_results, host='duck', subtypes=['H5N1', 'H5N2', 'H5N3', 'H5N6', 'H5N8'], min_y=0.5, max_y=0.8)
+    plot_host(cai_results, host='human', subtypes=['H5N1', 'H5N6'], min_y=0.65, max_y=0.8)
+    plot_host(cai_results, host='swine', subtypes=['H5N1', 'H5N6'], min_y=0.5, max_y=0.65)
 
-# Writes dataframe to xlsx detailing sequence counts for subtype hosts and genes.
-def seq_count_to_xlsx(cai_results): 
-    write_path = "data/plots/sequence_table.xlsx"
-    cai_results['subtype_host'] = cai_results['subtype'] + " " + cai_results['host'] # Combine subtype and hosts into one column
-    cai_results = cai_results[cai_results["gene"].str.contains("No gene") == False] # Exclude genes: No gene. Not enough data to include.
-    cai_results = cai_results.sort_values(['subtype', 'host', 'gene']) # sort subtype, host then gene in ascending order
-    seq_count = pd.DataFrame(columns=pd.unique(cai_results['gene']), index=pd.unique(cai_results['subtype_host']))
-    for gene in pd.unique(cai_results['gene']): # loop through list of unique genes
-        for subtype_host in pd.unique(cai_results['subtype_host']): # loop through list of unique subtype_hosts
-            df = cai_results[cai_results["gene"].str.contains(gene) == True] # filter for gene
-            df = df[df["subtype_host"].str.contains(subtype_host) == True] # filter for subtype_host
-            seq_count.loc[subtype_host, gene] = df.index.size # add sequence count to dataframe
-    utils.df_to_xlsx(seq_count, write_path)
+# Plots boxplots for each gene in a specific host.
+# Genes: HA, NA, NP, PA, PB1, and PB2.
+def plot_host(cai_results, host, subtypes, min_y, max_y):
+    cai_results = cai_results[cai_results["gene"].str.contains("No gene|MP|NS") == False] # exlcude no gene, MP, and NS genes.
+    cai_results = cai_results[cai_results["host"].str.contains(host) == True] # Select host 
+    cai_results = cai_results.sort_values(['gene']) # sort by gene
+    subtype_condition = ""
 
-# H5NX subtypes: H5N1, H5N2, H5N6, H5N8
-# Hosts: chicken
-# Genes: HA, NA, NP, PA, PB1, and PB2
-def chicken(cai_results):
-    cai_results = cai_results[cai_results["host"].str.contains("chicken") == True] # Select chicken
-    cai_results = cai_results.sort_values(['gene'])
+    for i in range(0, len(subtypes)): # Builds dataframe condition for selecting H5 subtypes
+
+        if i != len(subtypes) - 1:
+            subtype_condition += subtypes[i] + "|"
+
+        else:
+            subtype_condition += subtypes[i]
 
     for gene in pd.unique(cai_results['gene']):
-        write_path = 'data/plots/chicken_{0}.jpeg'.format(gene) # file destination
+        write_path = 'data/plots/{0}_{1}.jpeg'.format(host, gene) # file destination
         data = cai_results[cai_results["gene"].str.contains(gene) == True] # Filter for gene
-        data = data[data["subtype"].str.contains("H5N1|H5N2|H5N6|H5N8") == True] # Filter for subtypes
+        data = data[data["subtype"].str.contains(subtype_condition) == True] # Filter for subtypes
         data = data.sort_values(['subtype']) # sort subtype
 
-        title = gene
-        width = 0.5
-        fig_x_size = 7.5
-        fig_y_size = 7.5
-
-        stats = utils.test_chicken(data)
+        match host: # Chooses host specific method for calculating mann-whitney u test
+            case 'chicken':
+                host_test = utils.test_chicken(data)
+            case 'duck':
+                host_test = utils.test_duck(data)
+            case 'human':
+                host_test = utils.test_human(data)
+            case 'swine':
+                host_test = utils.test_swine(data)
 
         write_boxplot(
                         dataframe=data, 
+                        x='subtype',
+                        y='cai_score',
                         path=write_path, 
-                        stats=stats,
-                        fig_x_size=fig_x_size, 
-                        fig_y_size=fig_y_size, 
-                        width=width, 
-                        title=title, 
-                        min_y=0.70,
-                        max_y=0.9,
-                        hue='subtype'
-                        )
-        
-# H5NX subtypes: H5N1 and H5N6
-# Hosts: human
-# Genes: HA, NA, NP, PA, PB1, and PB2
-def human(cai_results):
-    cai_results = cai_results[cai_results["host"].str.contains("human") == True] # Select human
-
-    for gene in pd.unique(cai_results['gene']):
-        write_path = 'data/plots/human_{0}.jpeg'.format(gene) # file destination
-        data = cai_results[cai_results["gene"].str.contains(gene) == True] # Filter for gene
-        data = data[data["subtype"].str.contains("H5N1|H5N6") == True] # Filter for subtypes
-        data = data.sort_values(['subtype']) # sort subtype
-
-        title = gene
-        width = 0.5
-        fig_x_size = 7.5
-        fig_y_size = 7.5
-
-        stats = utils.test_human(data)
-
-        write_boxplot(  
-                        dataframe=data, 
-                        path=write_path, 
-                        stats=stats,
-                        fig_x_size=fig_x_size, 
-                        fig_y_size=fig_y_size, 
-                        width=width, 
-                        title=title, 
-                        min_y=0.65,
-                        max_y=0.80,
-                        hue='subtype'
-                        )
-        
-# H5NX subtypes: H5N1, H5N2, H5N3, H5N6, and H5N8
-# Hosts: duck
-# Genes: HA, NA, NP, PA, PB1, and PB2
-def duck(cai_results):
-    cai_results = cai_results[cai_results["host"].str.contains("duck") == True] # Select chicken
-
-    for gene in pd.unique(cai_results['gene']):
-        write_path = 'data/plots/duck_{0}.jpeg'.format(gene) # file destination
-        data = cai_results[cai_results["gene"].str.contains(gene) == True] # Filter for gene
-        data = data[data["subtype"].str.contains("H5N1|H5N2|H5N3|H5N6|H5N8") == True] # Filter for subtypes
-        data = data.sort_values(['subtype']) # sort subtype
-
-        title = gene
-        width = 0.5
-        fig_x_size = 7.5
-        fig_y_size = 7.5
-
-        stats = utils.test_duck(data)
-
-        write_boxplot(  
-                        dataframe=data, 
-                        path=write_path, 
-                        stats=stats,
-                        fig_x_size=fig_x_size, 
-                        fig_y_size=fig_y_size, 
-                        width=width, 
-                        title=title, 
-                        min_y=0.5,
-                        max_y=0.8,
-                        hue='subtype'
-                        )
-        
-# H5NX subtypes: H5N1 and H5N6
-# Hosts: swine
-# Genes: HA, NA, NP, PA, PB1, and PB2
-def swine(cai_results):
-    cai_results = cai_results[cai_results["host"].str.contains("swine") == True] # Select chicken
-
-    for gene in pd.unique(cai_results['gene']):
-        write_path = 'data/plots/swine_{0}.jpeg'.format(gene) # file destination
-        data = cai_results[cai_results["gene"].str.contains(gene) == True] # Filter for gene
-        data = data[data["subtype"].str.contains("H5N1|H5N6") == True] # Filter for subtypes
-        data = data.sort_values(['subtype']) # sort subtype
-
-        title = gene
-        width = 0.5
-        fig_x_size = 7.5
-        fig_y_size = 7.5
-
-        stats = utils.test_swine(data)
-
-        write_boxplot(  
-                        dataframe=data, 
-                        path=write_path, 
-                        stats=stats,
-                        fig_x_size=fig_x_size, 
-                        fig_y_size=fig_y_size, 
-                        width=width, 
-                        title=title, 
-                        min_y=0.5,
-                        max_y=0.65,
+                        stats=host_test,
+                        fig_x_size=7.5, 
+                        fig_y_size=7.5, 
+                        title=gene, 
+                        min_y=min_y,
+                        max_y=max_y,
                         hue='subtype'
                         )
 
@@ -163,9 +64,10 @@ def write_boxplot(
                     dataframe, 
                     path, 
                     stats,
+                    x,
+                    y,
                     fig_x_size, 
                     fig_y_size, 
-                    width, 
                     title, 
                     min_y=0,
                     max_y=1,
@@ -176,12 +78,12 @@ def write_boxplot(
 
     ax = sns.boxplot(
                         dataframe, 
-                        x='subtype', 
-                        y='cai_score', 
+                        x=x, 
+                        y=y, 
                         hue=hue, 
                         showfliers=False, 
                         palette="Set1", 
-                        width=width
+                        width=0.5
                         )
     
     starbars.draw_annotation(stats, ns_show=False, fontsize=20, bar_gap=0.15)
