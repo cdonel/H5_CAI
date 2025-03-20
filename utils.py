e1b3bb20890ab8f6bac8db6cb29f8908d633f14a
cdonel <88918165+cdonel@users.noreply.github.com>
2025-03-20 00:30:03 -0400

    method for standardized sequence description

--- utils.py
+++ utils.py
@@ -167,130 +167,22 @@ def csv_to_df(path):
     return pd.read_csv(path, keep_default_na=False, na_values=[None])
 
 # Scores to dataframe
-def cai_scores_to_df(scores, subtype, host, genes):
-    df = pd.DataFrame(columns=['cai_score'], data=scores)
-    df['subtype'] = subtype
-    df['host'] = host
-    df['gene'] = genes
+def cai_scores_to_df(scores, seq_records):
+    subtype = []
+    gene = []
+    host = []
+    if len(scores) == len(seq_records):
+        for record in seq_records:
+            subtype.append(re.search(r'subtype=(.*?)\|', record.description).group(1))
+            host.append(re.search(r'host=(.*?)\|', record.description).group(1))
+            gene.append(re.search(r'gene=(.*?)\|', record.description).group(1))
+
+    df = pd.DataFrame({'cai_score':scores, 'gene':gene, 'host':host, 'subtype':subtype})
     return df
 
 # Takes list of dataframes and concatenates into single datafarame
 def concat_df(list_df):
     return pd.concat(list_df, ignore_index=True)
-
-# Extracts gene name from sequence description and returns list 
-def get_H5_gene_names(seq_records):
-    genes = [] # empty list for holding gene name ex. HA
-    for record in seq_records:
-        x =  re.split(r".*?\(.*?\(.*?\)\)\s", record.description) # split everything before gene name
-        x = list(filter(None, x)) # remove empty strings from list
-        y = re.split(r',\s(?:partial|complete)\scds', x[0]) # split everything after gene name
-        y = list(filter(None, y)) # remove empty strings from list
-        gene = y[0]
-        
-        if re.search('hemagglutinin', gene, re.IGNORECASE): # if hemagglutinin is found in gene append HA to genes list
-            genes.append('HA')
-
-        elif re.search('(HA)', gene, re.IGNORECASE): # if (HA) is found in gene append HA to genes list
-            genes.append('HA')
-
-        elif re.search('neuraminidase', gene, re.IGNORECASE): # if neuraminidase is found in gene append NA to genes list
-            genes.append('NA')
-
-        elif re.search('(NA)', gene, re.IGNORECASE): # if (NA) is found in gene append NA to genes list
-            genes.append('NA')
-
-        elif re.search('matrix protein', gene, re.IGNORECASE): # if matrix protein is found in gene append MP to genes list
-            genes.append('MP')
-
-        elif re.search('structural', gene, re.IGNORECASE): # if structural is found in gene append NS to genes list
-            genes.append('NS')
-
-        elif re.search('PB1', gene, re.IGNORECASE): # if PB1 is found in gene append PB1 to genes list
-            genes.append('PB1')
-
-        elif re.search('PB2', gene, re.IGNORECASE): # if PB2 is found in gene append PB2 to genes list
-            genes.append('PB2')
-
-        elif re.search('(PA)', gene, re.IGNORECASE): # if PA is found in gene append PA to genes list
-            genes.append('PA')
-
-        elif re.search('(NP)', gene, re.IGNORECASE): # if NP is found in gene append NP to genes list
-            genes.append('NP')
-            
-        else:
-            genes.append("No gene") # if no gene was found above add No gene to genes list
-
-    return genes
-
-# Standardize host name.
-# Supported host types: duck, chicken, swine, bovine, and human.
-def standarize_host(record_description, 
-                    host_name,
-                    duck_keywords=duck_keywords,
-                    duck_host_names=duck_host_names,
-                    chicken_keywords=chicken_keywords,
-                    chicken_host_names=chicken_host_names,
-                    bovine_keywords=bovine_keywords,
-                    bovine_host_names=bovine_host_names,
-                    swine_host_names=swine_host_names,
-                    human_host_names=human_host_names):
-    
-    # Check if host name matches ANY substring in duck_keywords
-    # Change section of record descrition from 'Influenza A virus' to 'Influenza A virus duck'
-    for keyword in duck_keywords:
-        if keyword.lower() in host_name.lower():
-            record_description = re.sub(r'Influenza A virus', 'Influenza A virus duck', record_description)
-            return record_description
-        
-    # Check if host name matches any name in duck_host_names
-    # Change section of record descrition from 'Influenza A virus' to 'Influenza A virus duck'
-    for name in duck_host_names:
-        if name.lower() == host_name.lower():
-            record_description = re.sub(r'Influenza A virus', 'Influenza A virus duck', record_description)
-            return record_description
-        
-    # Check if host name matches ANY substring in chicken_keywords
-    # Change section of record descrition from 'Influenza A virus' to 'Influenza A virus chicken'
-    for keyword in chicken_keywords:
-        if keyword.lower() in host_name.lower():
-            record_description = re.sub(r'Influenza A virus', 'Influenza A virus chicken', record_description)
-            return record_description
-        
-    # Check if host name matches any name in chicken_host_names
-    # Change section of record descrition from 'Influenza A virus' to 'Influenza A virus chicken'
-    for name in chicken_host_names:
-        if name.lower() == host_name.lower():
-            record_description = re.sub(r'Influenza A virus', 'Influenza A virus chicken', record_description)
-            return record_description
-
-    # Check if host name matches ANY substring in bovine_keywords
-    # Change section of record descrition from 'Influenza A virus' to 'Influenza A virus bovine'
-    for keyword in bovine_keywords:
-        if keyword.lower() in host_name.lower():
-            record_description = re.sub(r'Influenza A virus', 'Influenza A virus bovine', record_description)
-            return record_description
-        
-    # Check if host name matches any name in bovine_host_names
-    # Change section of record descrition from 'Influenza A virus' to 'Influenza A virus bovine'
-    for name in bovine_host_names:
-        if name.lower() == host_name.lower():
-            record_description = re.sub(r'Influenza A virus', 'Influenza A virus bovine', record_description)
-            return record_description
-
-    # Check if host name matches any name in swine_host_names
-    # Change section of record descrition from 'Influenza A virus' to 'Influenza A virus swine'
-    for name in swine_host_names:
-        if name.lower() == host_name.lower():
-            record_description = re.sub(r'Influenza A virus', 'Influenza A virus swine', record_description)
-            return record_description
-
-    # Check if host name matches any name in human_host_names
-    # Change section of record descrition from 'Influenza A virus' to 'Influenza A virus human'
-    for name in human_host_names:
-        if name.lower() == host_name.lower():
-            record_description = re.sub(r'Influenza A virus', 'Influenza A virus human', record_description)
-            return record_description
 
 # Goes through each cleaned fasta file for each H5 subtypes and extracts gene name, subtype, and accession
 # Writes dataframe to csv
@@ -472,7 +364,6 @@ def test_swine(dataframe):
 def seq_count_to_xlsx(cai_results): 
     write_path = "data/plots/sequence_table.xlsx"
     cai_results['subtype_host'] = cai_results['subtype'] + " " + cai_results['host'] # Combine subtype and hosts into one column
-    cai_results = cai_results[cai_results["gene"].str.contains("No gene") == False] # Exclude genes: No gene. Not enough data to include.
     cai_results = cai_results.sort_values(['subtype', 'host', 'gene']) # sort subtype, host then gene in ascending order
     seq_count = pd.DataFrame(columns=pd.unique(cai_results['gene']), index=pd.unique(cai_results['subtype_host']))
 
@@ -484,3 +375,197 @@ def seq_count_to_xlsx(cai_results):
             seq_count.loc[subtype_host, gene] = df.index.size # add sequence count to dataframe
             
     df_to_xlsx(seq_count, write_path)
+
+#####################################################
+
+# searches and extracts location in sequence record
+def extract_location(record):
+    try:
+        pattern = r'.*?\(A/.*?/(.*?)/'
+        location = re.search(pattern, record.description).group(1)
+    
+    except:
+        location = 'None'
+    
+    return location
+
+# searches and extracts host in sequence record
+def extract_host(record):
+    try:
+        pattern = r'A/(.*?)/'
+        host = re.search(pattern, record.description).group(1)
+
+    except:
+        host = 'None'
+
+    return host
+
+# searches and extracts subtype in sequence record
+def extract_subtype(record):
+    try:
+        pattern = r'(H\dN\d)'
+        subtype = re.search(pattern, record.description).group(1)
+
+    except:
+        subtype = 'None'
+        
+    return subtype
+
+# searches and extracts gene in sequence record
+def extract_gene(record):
+    try:
+        x =  re.split(r".*?\(.*?\(.*?\)\)\s", record.description) # split everything before gene name
+        x = list(filter(None, x)) # remove empty strings from list
+        y = re.split(r',\s(?:partial|complete)\scds', x[0]) # split everything after gene name
+        y = list(filter(None, y)) # remove empty strings from list
+        gene = y[0]
+
+        if re.search('hemagglutinin', gene, re.IGNORECASE): # if hemagglutinin is found in gene append HA to genes list
+            return 'HA'
+
+        elif re.search('(HA)', gene, re.IGNORECASE): # if (HA) is found in gene append HA to genes list
+            return 'HA'
+        
+        elif re.search('segment 4', gene, re.IGNORECASE): # if (HA) is found in gene append HA to genes list
+            return 'HA'
+
+        elif re.search('neuraminidase', gene, re.IGNORECASE): # if neuraminidase is found in gene append NA to genes list
+            return 'NA'
+
+        elif re.search('(NA)', gene, re.IGNORECASE): # if (NA) is found in gene append NA to genes list
+            return 'NA'
+        
+        elif re.search('sgement 6', gene, re.IGNORECASE): # if (NA) is found in gene append NA to genes list
+            return 'NA'
+
+        elif re.search('matrix protein', gene, re.IGNORECASE): # if matrix protein is found in gene append MP to genes list
+            return 'MP'
+        
+        elif re.search('segment 7', gene, re.IGNORECASE): # if matrix protein is found in gene append MP to genes list
+            return 'MP'
+
+        elif re.search('structural', gene, re.IGNORECASE): # if structural is found in gene append NS to genes list
+            return 'NS'
+        
+        elif re.search('segment 8', gene, re.IGNORECASE): # if structural is found in gene append NS to genes list
+            return 'NS'
+
+        elif re.search('PB1', gene, re.IGNORECASE): # if PB1 is found in gene append PB1 to genes list
+            return 'PB1'
+        
+        elif re.search('segment 2', gene, re.IGNORECASE): # if PB2 is found in gene append PB2 to genes list
+            return 'PB1'
+
+        elif re.search('PB2', gene, re.IGNORECASE): # if PB2 is found in gene append PB2 to genes list
+            return 'PB2'
+        
+        elif re.search('segment 1', gene, re.IGNORECASE): # if PB2 is found in gene append PB2 to genes list
+            return 'PB2'
+
+        elif re.search('(PA)', gene, re.IGNORECASE): # if PA is found in gene append PA to genes list
+            return 'PA'
+        
+        elif re.search('segment 3', gene, re.IGNORECASE): # if PA is found in gene append PA to genes list
+            return 'PA'
+
+        elif re.search('(NP)', gene, re.IGNORECASE): # if NP is found in gene append NP to genes list
+            return 'NP'
+        
+        elif re.search('segment 5', gene, re.IGNORECASE): # if NP is found in gene append NP to genes list
+            return 'NP'
+        
+        else:
+            return 'None'
+
+    except:
+        gene = 'None'
+
+    return gene
+
+# creates new sequence description containing only accession, subtype, gene, host, and location.
+def build_sequence_description(seq_records):
+    for record in seq_records:
+        subtype = extract_subtype(record)
+        host = extract_host(record)
+        length = len(record.seq)
+        standardized_host = standarize_host(host)
+        if standardized_host == 'human':
+            location = host
+        else:
+            location = extract_location(record)
+        gene = extract_gene(record)
+
+        record.description = '[subtype={0}|gene={1}|host={2}|location={3}|length={4}]'.format(subtype, gene, standardized_host, location, length)
+
+    return seq_records
+
+# Standardize host name.
+# Supported host types: duck, chicken, swine, bovine, and human.
+def standarize_host(host,
+                    duck_keywords=duck_keywords,
+                    duck_host_names=duck_host_names,
+                    chicken_keywords=chicken_keywords,
+                    chicken_host_names=chicken_host_names,
+                    bovine_keywords=bovine_keywords,
+                    bovine_host_names=bovine_host_names,
+                    swine_host_names=swine_host_names,
+                    human_host_names=human_host_names):
+    
+    # Check if host name matches ANY substring in duck_keywords
+    # Return duck
+    for keyword in duck_keywords:
+        if keyword.lower() in host.lower():
+            return 'duck'
+        
+    # Check if host name matches any name in duck_host_names
+    # Return duck
+    for name in duck_host_names:
+        if name.lower() == host.lower():
+            return 'duck'
+        
+    # Check if host name matches ANY substring in chicken_keywords
+    # Return chicken
+    for keyword in chicken_keywords:
+        if keyword.lower() in host.lower():
+            return 'chicken'
+        
+    # Check if host name matches any name in chicken_host_names
+    # Return chicken
+    for name in chicken_host_names:
+        if name.lower() == host.lower():
+            return 'chicken'
+
+    # Check if host name matches ANY substring in bovine_keywords
+    # Return bovine
+    for keyword in bovine_keywords:
+        if keyword.lower() in host.lower():
+            return 'bovine'
+        
+    # Check if host name matches any name in bovine_host_names
+    # Return bovine
+    for name in bovine_host_names:
+        if name.lower() == host.lower():
+            return 'bovine'
+
+    # Check if host name matches any name in swine_host_names
+    # Return swine
+    for name in swine_host_names:
+        if name.lower() == host.lower():
+            return 'swine'
+
+    # Check if host name matches any name in human_host_names
+    # Return human
+    for name in human_host_names:
+        if name.lower() == host.lower():
+            return 'human'
+        
+# If subtype = None and host = None then do not add to new seq records.
+def remove_sequences(seq_records):
+    new_seq_records = []
+    for record in seq_records:
+        if re.search(r'host=None', record.description) or re.search(r'subtype=None', record.description):
+            pass
+        else:
+            new_seq_records.append(record)
+
+    return new_seq_records
