bc29141c68f681f54bb0683da33e7eb44428391a
cdonel <88918165+cdonel@users.noreply.github.com>
2025-03-20 00:52:22 -0400

    changed output directory from test to main

--- 10_retrieve_codon_usage_index.py
+++ 10_retrieve_codon_usage_index.py
@@ -4,7 +4,7 @@ import utils
 
 def main():
     for host, url in utils.codon_usage_index_urls.items(): # Loops through dictionary containing host name and URL
-        write_path = "data/codon_usage_database/test/{0}.txt".format(host) # Destination of output fil
+        write_path = "data/codon_usage_database/{0}.txt".format(host) # Destination of output fil
         condon_usage_index = get_codon_usage_index(url) # Codon usage index scraped from web data using URL
         utils.write_txt(write_path, condon_usage_index)
         print("Retrieved {0} codon usage index.".format(host))
